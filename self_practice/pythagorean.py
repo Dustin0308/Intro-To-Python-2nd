58c8d1d2f6df128c7b2d0a9ca4970b9a432ace04
Dustin0308 <dustinreaver@gmail.com>
2025-06-29 00:55:34 +0000

    Cleaned up pythagorean.py code.

--- self_practice/pythagorean.py
+++ self_practice/pythagorean.py
@@ -3,12 +3,12 @@ import math
 
 leg_1 = float(input('Please input A: '))
 leg_2 = float(input('Please input B: '))
-result = math.sqrt((leg_1)**2 + (leg_2)**2)
+result = math.sqrt((leg_1**2) + (leg_2**2))
 print(f'{result} is the length of your hypotenuse.')
 
 # Using a function
 
 def hypotenuse(leg_1, leg_2):
-    return math.sqrt(((leg_1)**2 + (leg_2**2)))
+    return math.sqrt((leg_1**2) + (leg_2**2))
 
-print(f'This is the length of your hypotenuse: ', (hypotenuse(3, 4)))
+print(f'This is the length of your hypotenuse: {hypotenuse(3, 4)}')
