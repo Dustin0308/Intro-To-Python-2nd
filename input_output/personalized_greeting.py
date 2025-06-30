15bce4c94faa644002f6cbd2f8f904cdbc49a0c1
Dustin0308 <dustinreaver@gmail.com>
2025-06-30 21:53:15 +0000

    Input/Output exercises.

---
+++ input_output/personalized_greeting.py
@@ -0,0 +1,17 @@
+print("What's your name?")
+name = input()
+
+print(f'Good Morning, {name}!')
+
+
+# OR
+
+name = input("What's your name? ")
+print(f'Good Morning, {name}!')
+
+
+# Outputting newline instead of a space
+
+name = input("What's your name?\n")
+
+print(f'Good Morning, {name}!')
